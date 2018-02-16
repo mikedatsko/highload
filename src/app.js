9cb7546f254bc277599664b976b9cb4800a531c3
Mykhailo Datsko <mykhailo.datsko@solidopinion.com>
2018-02-16 12:07:20 +0200

    added iterations, added adding form

--- src/app.js
+++ src/app.js
@@ -13,7 +13,6 @@ import {
   PreviewFrame,
   NavBar,
   NavBarMenu,
-  NavBarMenuItem,
   Content,
   Tools,
   ToolLogo,
@@ -21,6 +20,8 @@ import {
   Url,
   Logo
 } from './app.styles'
+
+const hostUrl = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : ''
 
 function sendMessage(e) {
   // Prevent any default browser behaviour.
@@ -33,6 +34,8 @@ function sendMessage(e) {
   // receiver.contentWindow;
   // receiver.postMessage('Hello Treehouse!', 'http://localhost:3000');
 }
+
+// window.parent.postMessage('FRAME_LOADED',(new URL(document.location.href)).searchParams.get('host_url') || 'http://jsmeasure.surge.sh');
 
 const timer = {
   start: 0,
@@ -49,73 +52,109 @@ export default class App extends Component {
       isFinished: false,
       isStarted: false,
       previewFrameSrc: '',
+      iterations: 1,
+      iteration: 1,
       participants: [
         {
           title: 'ES5',
           // url: 'http://localhost:8000',
-          url: 'http://jsmeasure-es5.surge.sh',
+          url: `http://jsmeasure-es5.surge.sh${hostUrl ? `?host_url=${hostUrl}` : ''}`,
           progress: 0,
           time: 0,
-          isSelected: false
+          times: [],
+          isSelected: false,
+          order: 0
         },
         {
           title: 'ES6',
           // url: 'http://localhost:8001',
-          url: 'http://jsmeasure-es6.surge.sh',
+          url: `http://jsmeasure-es6.surge.sh${hostUrl ? `?host_url=${hostUrl}` : ''}`,
           progress: 0,
           time: 0,
-          isSelected: false
+          times: [],
+          isSelected: false,
+          order: 1
         },
         {
           title: 'jQuery',
           // url: 'http://localhost:8002',
-          url: 'http://jsmeasure-jquery.surge.sh',
+          url: `http://jsmeasure-jquery.surge.sh${hostUrl ? `?host_url=${hostUrl}` : ''}`,
           progress: 0,
           time: 0,
-          isSelected: false
+          times: [],
+          isSelected: false,
+          order: 2
         },
         {
           title: 'Backbone',
           // url: 'http://localhost:8003',
-          url: 'http://jsmeasure-backbone.surge.sh',
+          url: `http://jsmeasure-backbone.surge.sh${hostUrl ? `?host_url=${hostUrl}` : ''}`,
           progress: 0,
           time: 0,
-          isSelected: false
+          times: [],
+          isSelected: false,
+          order: 3
         },
         {
           title: 'Aurelia',
           // url: 'http://localhost:7000',
           // url: 'http://localhost:9000#10000',
-          url: 'http://jsmeasure-aurelia.surge.sh',
+          url: `http://jsmeasure-aurelia.surge.sh${hostUrl ? `?host_url=${hostUrl}` : ''}`,
           progress: 0,
           time: 0,
-          isSelected: false
+          times: [],
+          isSelected: false,
+          order: 4
         },
         {
           title: 'Angular',
-          url: 'http://jsmeasure-angular.surge.sh',
+          url: `http://jsmeasure-angular.surge.sh${hostUrl ? `?host_url=${hostUrl}` : ''}`,
           // url: 'http://localhost:9000#10000',
-          // url: 'http://jsmeasure-aurelia.surge.sh',
           progress: 0,
           time: 0,
-          isSelected: false
+          times: [],
+          isSelected: false,
+          order: 5
         },
         {
           title: 'Angular 2',
-          url: 'http://jsmeasure-angular2.surge.sh',
+          url: `http://jsmeasure-angular2.surge.sh${hostUrl ? `?host_url=${hostUrl}` : ''}`,
           // url: 'http://localhost:9000#10000',
-          // url: 'http://jsmeasure-aurelia.surge.sh',
           progress: 0,
           time: 0,
-          isSelected: false
+          times: [],
+          isSelected: false,
+          order: 6
+        },
+        {
+          title: 'React',
+          url: `http://jsmeasure-react.surge.sh${hostUrl ? `?host_url=${hostUrl}` : ''}`,
+          // url: 'http://localhost:9000#10000',
+          progress: 0,
+          time: 0,
+          times: [],
+          isSelected: false,
+          order: 7
+        },
+        {
+          title: 'React (memory game)',
+          url: `http://jsmeasure-react-memory.surge.sh${hostUrl ? `?host_url=${hostUrl}` : ''}`,
+          // url: 'http://localhost:9000#10000',
+          progress: 0,
+          time: 0,
+          times: [],
+          isSelected: false,
+          order: 8
         }
-      ]
+      ],
+      add_url: '',
+      add_title: ''
     }
 
     window.addEventListener('message', event => {
       const message = event.data
 
-      console.log('message', message)
+      // console.log('message', message)
 
       if (message === 'FRAME_LOADED') {
         const result = new Date().getTime() - timer.start
@@ -126,18 +165,29 @@ export default class App extends Component {
 
           if (participant.isSelected) {
             participant.time = result
-            participant.isSelected = false
+            participant.times.push(result)
+            const iteration = +this.state.iteration
+            const iterations = +this.state.iterations
 
-            const participantNext = participants[i + 1]
+            // console.log(iteration, iterations)
 
-            if (participantNext) {
-              participantNext.isSelected = true
+            if (iteration < iterations) {
+              this.startTest()
+              this.setState({iteration: iteration + 1})
+            } else {
+              participant.isSelected = false
+
+              const participantNext = participants[i + 1]
+
+              if (participantNext) {
+                participantNext.isSelected = true
+              }
+
+              this.setState({participants: participants, iteration: 1})
+
+              this.startTest()
+              break
             }
-
-            this.setState({participants: participants})
-
-            this.startTest()
-            break
           }
         }
       }
@@ -145,10 +195,23 @@ export default class App extends Component {
   }
 
   startTest(isNewStart) {
+    this.setState({
+      isStarted: true,
+      previewFrameSrc: ''
+    })
+
     if (isNewStart) {
-      const participants = this.state.participants
+      // console.log(this.state.iterations)
+      const participants = this.state
+        .participants
+        .map(participant => {
+          participant.time = 0
+          return participant
+        })
+        .sort((a, b) => a.order > b.order)
+
       participants[0].isSelected = true
-      this.setState({participants: participants})
+      this.setState({participants: participants, isFinished: false})
     }
 
     timer.start = new Date().getTime()
@@ -160,7 +223,7 @@ export default class App extends Component {
       return
     }
 
-    console.log('participantSelected', participantSelected)
+    // console.log('participantSelected', participantSelected)
 
     this.setState({
       isStarted: true,
@@ -171,15 +234,45 @@ export default class App extends Component {
   }
 
   finishTest() {
-    const participants = this.state.participants.map(participant => {
-      participant.isSelected = false
-      return participant
-    })
+    // console.log(this.state.participants)
+
+    const participants = this.state
+      .participants
+      .map(participant => {
+        participant.isSelected = false
+        return participant
+      })
+      .sort((a, b) => a.time > b.time)
 
     this.setState({
       participants: participants,
       isStarted: false,
+      isFinished: true,
       previewFrameSrc: ''
+    })
+  }
+
+  onChangeInput(prop, value) {
+    this.setState({[prop]: value})
+  }
+
+  addTest() {
+    const participants = this.state.participants
+    const orderLast = Math.max.apply(Math, participants.map(participant => participant.order))
+    participants.push({
+      title: this.state.add_title,
+      url: `${this.state.add_url}${hostUrl ? `?host_url=${hostUrl}` : ''}`,
+      progress: 0,
+      time: 0,
+      times: [],
+      isSelected: false,
+      order: orderLast + 1
+    })
+
+    this.setState({
+      participants: participants,
+      add_title: '',
+      add_url: ''
     })
   }
 
@@ -205,16 +298,26 @@ export default class App extends Component {
   }
 
   renderParticipants() {
-    return this.state.participants.map((participant, i) => (
-      <tr key={'participant_' + i} className={participant.isSelected ? 'is-selected' : ''}>
-        <th>{i + 1}</th>
-        <td>
-          {participant.title} <Url href={participant.url} target={'_blank'}>{participant.url}</Url>
-        </td>
-        <td className="has-text-right">{participant.progress}%</td>
-        <td className="has-text-right">{participant.time}ms</td>
-      </tr>
-    ))
+    const participants = this.state.participants
+    const maxTime = Math.max.apply(Math, participants.map(participant => participant.time))
+
+    return participants.map((participant, i) => {
+      const width = Math.round(participant.time / maxTime * 100)
+      const time = participant.times.length
+        ? Math.round(participant.times.reduce((a, b) => a + b) / participant.times.length)
+        : participant.time
+
+      return (
+        <tr key={'participant_' + i} className={participant.isSelected ? 'is-selected' : ''}>
+          <th>{i + 1}</th>
+          <td className="participant-title">
+            {participant.title} <Url href={participant.url} target={'_blank'}>{participant.url}</Url>
+            <div className="participant-result" style={{width: `${width}%`}}/>
+          </td>
+          <td className="has-text-right">{participant.time}ms</td>
+        </tr>
+      )
+    })
   }
 
   render() {
@@ -230,9 +333,38 @@ export default class App extends Component {
               </div>
 
               <div className="navbar-end">
-                <NavBarMenuItem className="navbar-item">
-                  Home
-                </NavBarMenuItem>
+                <div className="add-form">
+                  <div className="field  is-grouped">
+                    <p className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="Title"
+                        value={this.state.add_title}
+                        onChange={e => this.onChangeInput('add_title', e.target.value)}
+                      />
+                    </p>
+
+                    <p className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="Url"
+                        value={this.state.add_url}
+                        onChange={e => this.onChangeInput('add_url', e.target.value)}
+                      />
+                    </p>
+
+                    <p className="control">
+                      <a
+                        className={'button is-link' + (this.state.isStarted ? ' is-loading' : '')}
+                        onClick={() => this.addTest()}
+                      >
+                        Add
+                      </a>
+                    </p>
+                  </div>
+                </div>
               </div>
             </NavBarMenu>
           </NavBar>
@@ -241,19 +373,41 @@ export default class App extends Component {
             <div className="container">
               <div className="columns">
                 <div className="column">
-                  <a
-                    className={'button is-primary is-pulled-right' + (this.state.isStarted ? ' is-loading' : '')}
-                    onClick={() => this.startTest(true)}
-                  >
-                    Start test
-                  </a>
+                  <div className="field is-grouped">
+                    <p className="control">
+                      <a className="button is-white">
+                        Number of iterations
+                      </a>
+                    </p>
+
+                    <p className="control">
+                      <div className="select">
+                        <select name="country" onChange={e => this.onChangeInput('iterations', e.target.value)}>
+                          <option value="1">1</option>
+                          <option value="10">10</option>
+                          <option value="100">100</option>
+                          <option value="1000">1000</option>
+                          <option value="10000">10000</option>
+                          <option value="100000">100000</option>
+                        </select>
+                      </div>
+                    </p>
+
+                    <p className="control">
+                      <a
+                        className={'button is-primary' + (this.state.isStarted ? ' is-loading' : '')}
+                        onClick={() => this.startTest(true)}
+                      >
+                        Start
+                      </a>
+                    </p>
+                  </div>
 
                   <table className="table">
                     <thead>
                       <tr>
                         <th width="50">#</th>
                         <th>Title</th>
-                        <th width="100">Progress</th>
                         <th width="100">Time, ms</th>
                       </tr>
                     </thead>
